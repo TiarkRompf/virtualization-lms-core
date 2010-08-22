de60586fc31d364ed882de94f910e8fe5bab640a
unknown <kuubemasta@.(none)>
2010-08-22 15:50:15 -0700

    Added a small comment to test out my git setup.

--- src/common/Base.scala
+++ src/common/Base.scala
@@ -3,6 +3,12 @@ package common
 
 import internal.{Expressions, Effects, ScalaCodegen, ScalaNestedCodegen }
 
+/**
+ * The Base trait defines the type Rep, which is the higher-ordered kind that allows for other DSL types to be
+ * polymorphically embedded.
+ *
+ *  
+ */
 trait Base extends EmbeddedControls {
 
   type Rep[+T]
