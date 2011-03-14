a365514e137f4cd6d782db95b18f21cd211eead9
asujeeth <asujeeth@stanford.edu>
2011-03-13 22:26:02 -0700

    fix bug with string concatenation

--- src/common/StringOps.scala
+++ src/common/StringOps.scala
@@ -14,10 +14,11 @@ trait LiftString {
 trait StringOps extends Variables with OverloadHack {
   // NOTE: if something doesn't get lifted, this won't give you a compile time error,
   //       since string concat is defined on all objects
-  def infix_+(s1: String, s2: Rep[Any]) = string_plus(unit(s1),s2)
-  def infix_+(s1: Rep[Any], s2: String)(implicit o: Overloaded1) = string_plus(s1,unit(s2))
-  def infix_+(s1: String, s2: Var[Any])(implicit o: Overloaded4) = string_plus(unit(s1),s2)
-  def infix_+(s1: Var[Any], s2: String)(implicit o: Overloaded5) = string_plus(s1,unit(s2))
+  def infix_+(s1: Rep[String], s2: Rep[Any]) = string_plus(s1, s2)
+  def infix_+(s1: Rep[String], s2: Var[Any])(implicit o: Overloaded1) = string_plus(s1, readVar(s2))
+  def infix_+(s1: Rep[String], s2: Any)(implicit o: Overloaded2) = string_plus(s1, unit(s2))
+  def infix_+(s1: Rep[Any], s2: String)(implicit o: Overloaded3) = string_plus(s1, unit(s2))
+  def infix_+(s1: Var[Any], s2: String)(implicit o: Overloaded4) = string_plus(readVar(s1), unit(s2))
 
   def infix_trim(s: Rep[String]) = string_trim(s)
   def infix_split(s: Rep[String], separators: Rep[String]) = string_split(s, separators)
