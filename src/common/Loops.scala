646b241ee24aeb7afbb4be29ed2f80177ac69853
Tiark Rompf <tiark.rompf@epfl.ch>
2011-03-28 14:48:45 +0200

    code motion test case

--- src/common/Loops.scala
+++ src/common/Loops.scala
@@ -38,7 +38,7 @@ trait BaseGenLoops extends GenericNestedCodegen {
   import IR._
 
   override def syms(e: Any): List[Sym[Any]] = e match {
-    case e: AbstractLoop[_] => syms(e.size) ::: syms(e.body) // should add super.syms(e) ??
+    case e: AbstractLoop[_] => syms(e.size) ::: syms(e.body) // should add super.syms(e) ?? not without a flag ...
     case _ => super.syms(e)
   }
 
--- test-src/epfl/test7-analysis/TestCodeMotion.scala
+++ test-src/epfl/test7-analysis/TestCodeMotion.scala
@@ -26,6 +26,35 @@ trait NestLambdaProg extends Arith with Functions with Print { // also used by T
   
 }
 
+trait NestCondProg extends Arith with Functions with IfThenElse with Print {
+  
+  /* TODO: This program exhibits behavior that is likely undesired in many
+  cases. The definition of f will be moved *into* g and into the conditional.
+  The doLambda in the else branch will not be hoisted out of g either.
+  
+  While there are situations where this particular kind of code motion
+  is an improvement (namely, if the probability of y == true is very low
+  and the else branch would be cheap).
+  */
+  
+  
+  def test(x: Rep[Unit]) = {
+    val f = doLambda { x: Rep[Double] => 2 * x }
+    
+    val g = doLambda { y: Rep[Boolean] =>
+      print("yo")
+      if (y)
+        f
+      else
+        doLambda { x: Rep[Double] => x + 1 }
+   }
+    g
+  }
+  
+}
+
+
+
 
 class TestCodemotion extends FileDiffSuite {
   
@@ -41,5 +70,17 @@ class TestCodemotion extends FileDiffSuite {
     }
     assertFileEqualsCheck(prefix+"codemotion1")
   }
+
+  def testCodemotion2 = {
+    // test loop hoisting (should use loops but lambdas will do for now)
+    withOutFile(prefix+"codemotion2") {
+      new NestCondProg with ArithExp with FunctionsExp with IfThenElseExp with PrintExp { self =>
+        val codegen = new ScalaGenArith with ScalaGenFunctions with ScalaGenIfThenElse with ScalaGenPrint { val IR: self.type = self }
+        codegen.emitSource(test, "Test", new PrintWriter(System.out))
+      }
+    }
+    assertFileEqualsCheck(prefix+"codemotion2")
+  }
+
 
 }
