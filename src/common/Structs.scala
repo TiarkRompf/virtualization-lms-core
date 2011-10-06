d5d63e260155452b6a05342dbdf11c694ecf6936
Tiark Rompf <tiark.rompf@epfl.ch>
2011-10-06 11:59:16 +0200

    copy TestStruct -> common

---
+++ src/common/Structs.scala
@@ -0,0 +1,441 @@
+package scala.virtualization.lms
+package epfl
+package test9
+
+import common._
+import internal.{FatExpressions,GenericFatCodegen}
+
+
+import test1._
+import test7.{Print,PrintExp,ScalaGenPrint}
+import test7.{ArrayLoops,ArrayLoopsExp,ArrayLoopsFatExp,ScalaGenArrayLoops,ScalaGenFatArrayLoopsFusionOpt,TransformingStuff}
+
+import util.OverloadHack
+
+import java.io.{PrintWriter,StringWriter,FileOutputStream}
+
+
+trait ComplexArith extends Arith with ComplexBase with OverloadHack {
+  
+  def infix_+(x: Rep[Complex], y: Rep[Complex])(implicit o: Overloaded1): Rep[Complex] = Complex(x.re + y.re, x.im + y.im)
+  def infix_-(x: Rep[Complex], y: Rep[Complex])(implicit o: Overloaded1): Rep[Complex] = Complex(x.re - y.re, x.im - y.im)
+  //def infix_*(x: Rep[Complex], y: Rep[Complex]): Rep[Complex] = Complex(x.re + y.re, x.im + y.im)
+  
+}
+
+trait ComplexBase extends Arith {
+  
+  class Complex
+  
+  def Complex(re: Rep[Double], im: Rep[Double]): Rep[Complex]
+  def infix_re(c: Rep[Complex]): Rep[Double]
+  def infix_im(c: Rep[Complex]): Rep[Double]
+}
+
+trait ComplexStructExp extends ComplexBase with StructExp {
+
+  def Complex(re: Rep[Double], im: Rep[Double]) = struct[Complex](List("Complex"), Map("re"->re, "im"->im))
+  def infix_re(c: Rep[Complex]): Rep[Double] = field[Double](c, "re")
+  def infix_im(c: Rep[Complex]): Rep[Double] = field[Double](c, "im")
+  
+}
+
+
+// ------ struct impl follows, will move to common once stable
+
+trait StructExp extends BaseExp with VariablesExp with IfThenElseExp with ArrayLoopsExp {
+  
+  case class Struct[T](tag: List[String], elems: Map[String,Rep[Any]]) extends Def[T]
+  case class Field[T](struct: Rep[Any], index: String) extends Def[T]
+  
+  def struct[T:Manifest](tag: List[String], elems: Map[String,Rep[Any]]): Rep[T] = Struct[T](tag, elems)
+  
+  def field[T:Manifest](struct: Rep[Any], index: String): Rep[T] = struct match {
+    case Def(Struct(tag, elems)) => elems(index).asInstanceOf[Rep[T]]
+    case _ => Field[T](struct, index)
+  }
+  
+  // FIXME: need  syms override because Map is not a Product
+  override def syms(x: Any): List[Sym[Any]] = x match {
+    case z:Iterable[_] => z.toList.flatMap(syms)
+    case _ => super.syms(x)
+  }
+
+  override def symsFreq(e: Any): List[(Sym[Any], Double)] = e match {
+    case z:Iterable[_] => z.toList.flatMap(symsFreq)
+    case _ => super.symsFreq(e)
+  }  
+  
+  override def mirror[A:Manifest](e: Def[A], f: Transformer): Exp[A] = e match {
+    case Struct(tag, elems) => struct(tag, elems map { case (k,v) => (k, f(v)) })
+    case _ => super.mirror(e,f)
+  }
+  
+  // ----------------------
+  
+  override def var_new[T:Manifest](init: Exp[T]): Var[T] = init match {
+    case Def(Struct(tag, elems)) => 
+      //val r = Variable(struct(tag, elems.mapValues(e=>var_new(e).e))) // DON'T use mapValues!! <--lazy
+      Variable(struct[Variable[T]](tag, elems.map(p=>(p._1,var_new(p._2).e))))
+    case _ => 
+      super.var_new(init)
+  }
+
+  override def var_assign[T:Manifest](lhs: Var[T], rhs: Exp[T]): Exp[Unit] = (lhs,rhs) match {
+    case (Variable(Def(Struct(tagL,elemsL:Map[String,Exp[Variable[Any]]]))), Def(Struct(tagR, elemsR))) => 
+      assert(tagL == tagR)
+      assert(elemsL.keySet == elemsR.keySet)
+      for (k <- elemsL.keySet)
+        var_assign(Variable(elemsL(k)), elemsR(k))
+      Const(())
+    case _ => super.var_assign(lhs, rhs)
+  }
+  
+  override def readVar[T:Manifest](v: Var[T]) : Exp[T] = v match {
+    case Variable(Def(Struct(tag, elems: Map[String,Exp[Variable[Any]]]))) => 
+      struct[T](tag, elems.map(p=>(p._1,readVar(Variable(p._2)))))
+    case _ => super.readVar(v)
+  }
+  
+  
+  /*def reReify[T:Manifest](a: Rep[T]): Rep[T] = a match { // TODO: should work with loop bodies, too (Def!)
+    // TODO: this seems inherently unsafe because it duplicates effects. what should we do about it?
+    case Def(Reify(Def(Struct(tag,elems)),es,u)) =>
+      struct[T](tag, elems.map(p=>(p._1,toAtom(Reify(p._2, es, u))))) // result is struct(reify(...))
+    case _ => a
+  }
+  override def ifThenElse[T:Manifest](cond: Rep[Boolean], a: Rep[T], b: Rep[T]) = (reReify(a),reReify(b)) match {
+    case (Def(Struct(tagA,elemsA)), Def(Struct(tagB, elemsB))) => 
+      assert(tagA == tagB)
+      assert(elemsA.keySet == elemsB.keySet)
+      val elemsNew = for (k <- elemsA.keySet) yield (k -> ifThenElse(cond, elemsA(k), elemsB(k)))
+      struct[T](tagA, elemsNew.toMap)
+    case _ => super.ifThenElse(cond,a,b)
+  }*/
+
+
+  override def ifThenElse[T:Manifest](cond: Rep[Boolean], a: Rep[T], b: Rep[T]) = (a,b) match {
+    case (Def(Struct(tagA,elemsA)), Def(Struct(tagB, elemsB))) => 
+      assert(tagA == tagB)
+      assert(elemsA.keySet == elemsB.keySet)
+      val elemsNew = for (k <- elemsA.keySet) yield (k -> ifThenElse(cond, elemsA(k), elemsB(k)))
+      struct[T](tagA, elemsNew.toMap)
+    case _ => super.ifThenElse(cond,a,b)
+  }
+  
+  override def simpleLoop[A:Manifest](size: Exp[Int], v: Sym[Int], body: Def[A]): Exp[A] = body match {
+    case ArrayElem(Def(Struct(tag, elems))) => 
+      struct[A]("Array"::tag, elems.map(p=>(p._1,simpleLoop(size, v, ArrayElem(p._2)))))
+    case ArrayElem(Def(ArrayIndex(b,v))) if infix_length(b) == size => b.asInstanceOf[Exp[A]] // eta-reduce!
+    case _ => super.simpleLoop(size, v, body)
+  }
+  
+  override def infix_at[T:Manifest](a: Rep[Array[T]], i: Rep[Int]): Rep[T] = a match {
+    case Def(Struct(pre::tag,elems:Map[String,Exp[Array[T]]])) =>
+      assert(pre == "Array")
+      struct[T](tag, elems.map(p=>(p._1,infix_at(p._2, i))))
+    case _ => super.infix_at(a,i)
+  }
+  
+  override def infix_length[T:Manifest](a: Rep[Array[T]]): Rep[Int] = a match {
+    case Def(Struct(pre::tag,elems:Map[String,Exp[Array[T]]])) =>
+      assert(pre == "Array")
+      val ll = elems.map(p=>infix_length(p._2)) // all arrays must have same length!
+      ll reduceLeft { (a1,a2) => assert(a1 == a2); a1 }
+    case _ => super.infix_length(a)
+  }
+  
+}
+
+
+trait StructFatExp extends StructExp with IfThenElseFatExp { 
+
+  case class Phi[T](cond: Exp[Boolean], a1: Exp[Unit], val thenp: Exp[T], b1: Exp[Unit], val elsep: Exp[T])(val parent: Exp[Unit]) extends AbstractIfThenElse[T] // parent points to conditional
+  def phi[T:Manifest](c: Exp[Boolean], a1: Exp[Unit], a2: Exp[T], b1: Exp[Unit], b2: Exp[T])(parent: Exp[Unit]): Exp[T] = if (a2 == b2) a2 else Phi(c,a1,a2,b1,b2)(parent)
+
+  override def syms(x: Any): List[Sym[Any]] = x match {
+//    case Phi(c,a,u,b,v) => syms(List(c,a,b))
+    case _ => super.syms(x)
+  }
+
+  override def symsFreq(e: Any): List[(Sym[Any], Double)] = e match {
+//    case Phi(c,a,u,b,v) => freqNormal(c) ++ freqCold(a) ++ freqCold(b)
+    case _ => super.symsFreq(e)
+  }
+  
+  override def boundSyms(e: Any): List[Sym[Any]] = e match {
+    case Phi(c,a,u,b,v) => effectSyms(a):::effectSyms(b)
+    case _ => super.boundSyms(e)
+  }
+
+  override def mirror[A:Manifest](e: Def[A], f: Transformer): Exp[A] = e match {
+    case p@Phi(c,a,u,b,v) => phi(f(c),f(a),f(u),f(b),f(v))(f(p.parent))
+    case _ => super.mirror(e,f)
+  }
+
+  def deReify[T:Manifest](a: Rep[T]): (Rep[Unit], Rep[T]) = a match { // take Reify(stms, e) and return Reify(stms, ()), e
+    case Def(Reify(x,es,u)) => (toAtom(Reify(Const(()), es, u)), x)
+    case _ => (Const(()), a)
+  }
+  
+  
+  override def ifThenElse[T:Manifest](cond: Rep[Boolean], a: Rep[T], b: Rep[T]) = (deReify(a),deReify(b)) match {
+    case ((u, x@Def(Struct(tagA,elemsA))), (v, y@Def(Struct(tagB, elemsB)))) => 
+      assert(tagA == tagB)
+      assert(elemsA.keySet == elemsB.keySet)
+      // create stm that computes all values at once
+      // return struct of syms
+      val combinedResult = super.ifThenElse(cond,u,v)
+      
+      val elemsNew = for (k <- elemsA.keySet) yield (k -> phi(cond,u,elemsA(k),v,elemsB(k))(combinedResult))
+      println("----- " + combinedResult + " / " + elemsNew)
+      struct[T](tagA, elemsNew.toMap)
+      
+    case _ => super.ifThenElse(cond,a,b)
+  }
+
+
+
+}
+
+
+
+trait ScalaGenStruct extends ScalaGenBase {
+  val IR: StructExp
+  import IR._
+  
+  override def emitNode(sym: Sym[Any], rhs: Def[Any])(implicit stream: PrintWriter) = rhs match {
+    case Struct(tag, elems) => 
+      emitValDef(sym, "XXX " + rhs)
+    case Field(struct, index) =>  
+      emitValDef(sym, "XXX " + rhs)
+    case _ => super.emitNode(sym, rhs)
+  }
+}
+
+trait ScalaGenFatStruct extends ScalaGenStruct with GenericFatCodegen {
+  val IR: StructFatExp
+  import IR._
+  
+  override def emitNode(sym: Sym[Any], rhs: Def[Any])(implicit stream: PrintWriter) = rhs match {
+    case p@Phi(c,a,u,b,v) =>
+      emitValDef(sym, "XXX " + rhs + " // parent " + quote(p.parent))
+    case _ => super.emitNode(sym, rhs)
+  }
+  
+  
+  // TODO: implement regular fatten ?
+  
+  override def fattenAll(e: List[TP[Any]]): List[TTP] = {
+    val m = e collect { 
+      case t@TP(sym, p @ Phi(c,a,u,b,v)) => t
+    } groupBy { 
+      case TP(sym, p @ Phi(c,a,u,b,v)) => p.parent
+    }
+
+    //println("grouped: ")
+    //println(m.mkString("\n"))
+
+    def fatphi(s:Sym[Unit]) = {
+      val phis = m(s)
+      val ss = phis collect { case TP(s, _) => s }
+      val us = phis collect { case TP(_, Phi(c,a,u,b,v)) => u } // assert c,a,b match
+      val vs = phis collect { case TP(_, Phi(c,a,u,b,v)) => v }
+      val c  = phis collect { case TP(_, Phi(c,a,u,b,v)) => c } reduceLeft { (c1,c2) => assert(c1 == c2); c1 }
+      TTP(ss, SimpleFatIfThenElse(c,us,vs))
+    }
+    def fatif(s:Sym[Unit],c:Exp[Boolean],a:Exp[Unit],b:Exp[Unit]) = fatphi(s) match {
+      case TTP(ss, SimpleFatIfThenElse(c2,us,vs)) =>
+        assert(c == c2)
+        TTP(s::ss, SimpleFatIfThenElse(c,a::us,b::vs))
+    }
+
+    val orphans = m.keys.toList.filterNot(k => e exists (_.sym == k)) // parent if/else might have been removed!
+
+    val r = e.flatMap { 
+      case TP(sym, p@Phi(c,a,u,b,v)) => Nil
+      case TP(sym:Sym[Unit], IfThenElse(c,a:Exp[Unit],b:Exp[Unit])) => List(fatif(sym,c,a,b))
+      case TP(sym:Sym[Unit], Reflect(IfThenElse(c,a:Exp[Unit],b:Exp[Unit]),_,_)) => List(fatif(sym,c,a,b))
+      case t => List(fatten(t))
+    } ++ orphans.map { case s: Sym[Unit] => fatphi(s) }
+    
+    r.foreach(println)
+    r
+  }
+}
+
+
+// ----- test cases
+
+
+class TestStruct extends FileDiffSuite {
+  
+  val prefix = "test-out/epfl/test9-"
+  
+  trait DSL extends ComplexArith with ArrayLoops with Arith with OrderingOps with Variables with LiftVariables with IfThenElse with RangeOps with Print {
+    def infix_toDouble(x: Rep[Int]): Rep[Double] = x.asInstanceOf[Rep[Double]]
+    def test(x: Rep[Int]): Rep[Any]
+  }
+
+  trait Impl extends DSL with ComplexStructExp with ArrayLoopsExp with ArithExp with OrderingOpsExp with VariablesExp 
+      with IfThenElseExp with RangeOpsExp with PrintExp { self => 
+    override val verbosity = 2
+    val codegen = new ScalaGenArrayLoops with ScalaGenStruct with ScalaGenArith with ScalaGenOrderingOps 
+      with ScalaGenVariables with ScalaGenIfThenElse with ScalaGenRangeOps 
+      with ScalaGenPrint { val IR: self.type = self }
+    codegen.emitSource(test, "Test", new PrintWriter(System.out))
+  }
+
+  trait ImplFused extends DSL with ComplexStructExp with StructFatExp with ArrayLoopsFatExp with ArithExp with OrderingOpsExp with VariablesExp 
+      with IfThenElseExp with RangeOpsExp with PrintExp with TransformingStuff { self => 
+    override val verbosity = 2
+    val codegen = new ScalaGenFatArrayLoopsFusionOpt with ScalaGenFatStruct with ScalaGenArith with ScalaGenOrderingOps 
+      with ScalaGenVariables with ScalaGenIfThenElse with ScalaGenRangeOps 
+      with ScalaGenPrint { val IR: self.type = self;
+        override def shouldApplyFusion(currentScope: List[TTP])(result: List[Exp[Any]]): Boolean = true }
+    codegen.emitSource(test, "Test", new PrintWriter(System.out))
+  }
+
+  
+  
+  def testStruct1 = {
+    withOutFile(prefix+"struct1") {
+      // test variable splitting
+      trait Prog extends DSL {
+        def test(x: Rep[Int]) = {
+          var c = Complex(x.toDouble, 0)
+          c = c + Complex(0,x.toDouble)
+          print(c)
+        }
+      }
+      new Prog with Impl
+    }
+    assertFileEqualsCheck(prefix+"struct1")
+  }
+
+  def testStruct2 = {
+    withOutFile(prefix+"struct2") {
+      // test basic struct flattening (loops, variables, conditionals)
+      println("REMARK: this makes only sense with fat codegen (computation duplicated and some structs not removed otherwise)")
+      trait Prog extends DSL {
+        def test(x: Rep[Int]) = {
+          // split loops (rely on fusion, don't want to duplicate computation!)
+
+          val vector1 = array(100) { i => Complex(i.toDouble, 0.0 - i.toDouble) }
+          val vector2 = array(100) { i => Complex(0.0 - i.toDouble, i.toDouble) }
+
+          var vvar = vector2
+
+          // split conditionals (be careful about effects)
+
+          val vector3 = if (x > 7) vector1 else vvar
+
+          // conditional is reflected because it reads vvar -- effect ordering for split terms?
+
+          vvar = vector1
+
+          print(vvar)
+          print(vector3)
+        }
+      }
+      new Prog with Impl
+    }
+    assertFileEqualsCheck(prefix+"struct2")
+  }
+
+  def testStruct2b = {
+    withOutFile(prefix+"struct2b") {
+      // test basic struct flattening (loops, variables, conditionals)
+      trait Prog extends DSL {
+        def test(x: Rep[Int]) = {
+          // split loops (rely on fusion, don't want to duplicate computation!)
+
+          val vector1 = array(100) { i => Complex(i.toDouble, 0.0 - i.toDouble) }
+          val vector2 = array(100) { i => Complex(0.0 - i.toDouble, i.toDouble) }
+
+          var vvar = vector2
+
+          // split conditionals (be careful about effects)
+
+          val vector3 = if (x > 7) { 
+            print("foobar true")
+            vector1
+          } else {
+            print("foobar false")
+            vvar 
+          }
+
+          vvar = vector1
+
+          print(vvar)
+          print(vector3)
+        }
+      }
+      new Prog with ImplFused
+    }
+    assertFileEqualsCheck(prefix+"struct2b")
+  }
+
+  def testStruct3 = {
+    withOutFile(prefix+"struct3") {
+      // fuse conjugate computation with construction, essentially a no-op
+      trait Prog extends DSL {
+        def test(x: Rep[Int]) = {
+
+          val vector1 = array(100) { i => Complex(i.toDouble, 0.0 - i.toDouble) }
+
+          def conj(c: Rep[Complex]) = Complex(c.re, 0.0 - c.im)
+          def infix_map[A:Manifest,B:Manifest](c: Rep[Array[A]], f: Rep[A] => Rep[B]) = array(c.length) { i => f(c.at(i)) }
+
+          val vector3 = vector1.map(conj)
+
+          print(vector3)
+        }
+      }
+      new Prog with ImplFused {
+        // TODO: use a generic Opt trait instead of defining rewrites here...
+        override def infix_-(x: Exp[Double], y: Exp[Double]) = (x, y) match {
+          case (x, Def(Minus(Const(0.0),y))) => infix_+(x,y)
+          case _ => super.infix_-(x,y)
+        }
+        override def infix_+(x: Exp[Double], y: Exp[Double]) = (x, y) match {
+          case (Const(0.0), y) => y
+          case _ => super.infix_+(x,y)
+        }
+      }
+    }
+    assertFileEqualsCheck(prefix+"struct3")
+  }
+
+  def testStruct4 = {
+    withOutFile(prefix+"struct4") {
+      trait Prog extends DSL {
+        // recognize that only imaginary part is modified, real part untouched
+        def test(x: Rep[Int]) = {
+
+          val vector1 = array(100) { i => Complex(i.toDouble, 0.0 - i.toDouble) }
+          val vector2 = array(100) { i => Complex(0.0 - i.toDouble, i.toDouble) }
+
+          def conj(c: Rep[Complex]) = Complex(c.re, 0.0 - c.im)
+          def infix_map[A:Manifest,B:Manifest](c: Rep[Array[A]], f: Rep[A] => Rep[B]) = array(c.length) { i => f(c.at(i)) }
+
+          var vvar = vector1 // force access outside conditional, otherwise construction will be moved inside, defeating purpose of test
+
+          // result of this conditional should be a *single* array 
+          // containing the flattened im fields. re fields should be
+          // unconditional.
+          val vector3 = if (x > 7) {
+            vector1.map(conj)
+          } else {
+            vector1
+          }
+
+          print(vector3)
+        }
+      }
+      new Prog with ImplFused
+    }
+    assertFileEqualsCheck(prefix+"struct4")
+  }
+
+}
