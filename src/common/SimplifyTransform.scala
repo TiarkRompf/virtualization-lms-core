2bceb967da05f113e1b890a32ba6c2bd6bfccf40
Philipp Haller <hallerp@gmail.com>
2012-02-12 19:03:27 +0100

    Fix propagation of SourceContext in SimplifyTransform

--- src/common/SimplifyTransform.scala
+++ src/common/SimplifyTransform.scala
@@ -1,6 +1,8 @@
 package scala.virtualization.lms
 package common
 
+import scala.reflect.SourceContext
+
 // TODO: generalize and clean up
 
 trait SimplifyTransform extends internal.GenericFatCodegen {
@@ -13,10 +15,15 @@ trait SimplifyTransform extends internal.GenericFatCodegen {
     case Forward(x) => emitValDef(sym, quote(x))
     case _ => super.emitNode(sym, rhs)
   }
-  
+
+  def freshSourceContext()(implicit ctx: SourceContext) = ctx
+
   def transformOne[A](s: Sym[A], x: Def[A], t: SubstTransformer): Exp[A] = {
     if (t.subst.contains(s)) return t(s)
     implicit val m: Manifest[A] = s.Type.asInstanceOf[Manifest[A]]
+    val ctx: SourceContext =
+      if (s.sourceContexts.isEmpty) freshSourceContext()
+      else s.sourceContexts(0)
 
     //if (!syms(x).exists(t.subst contains _)) return s   //<---- should be safe to prune but test fails (??)
 
@@ -24,7 +31,7 @@ trait SimplifyTransform extends internal.GenericFatCodegen {
       val ss = syms(x)
       val tss = t(ss)
       if (ss != tss) {
-        val s2 = mirror(x, t) 
+        val s2 = mirror(x, t)(m, ctx)
         if (s2 == s) {
           printerr("warning: mirroring of "+s+"="+x+" syms " + ss.mkString(",") + " returned same object (expected t(syms) = " + tss.mkString(",") + ")")
         }
