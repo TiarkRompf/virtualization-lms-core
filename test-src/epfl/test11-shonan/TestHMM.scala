8d9e70ce611d3e8697b4e1396bf5d0e06bbe7704
Tiark Rompf <tiark.rompf@epfl.ch>
2012-06-13 09:03:48 +0200

    work in progress on stencils / cse across iterations

--- test-src/epfl/test11-shonan/TestHMM.scala
+++ test-src/epfl/test11-shonan/TestHMM.scala
@@ -14,35 +14,37 @@ import java.io.{PrintWriter,StringWriter,FileOutputStream}
 import scala.reflect.SourceContext
 
 
-trait DSL extends LiftNumeric with NumericOps with ArrayOps with RangeOps with BooleanOps 
-  with LiftVariables with IfThenElse with Print {
-  def staticData[T:Manifest](x: T): Rep[T]
-  def test(x: Rep[Array[Int]]): Rep[Array[Int]]
-}
-trait Impl extends DSL with Runner with ArrayOpsExpOpt with NumericOpsExpOpt with OrderingOpsExpOpt with BooleanOpsExp 
-    with EqualExpOpt with VariablesExpOpt with RangeOpsExp with StaticDataExp
-    with IfThenElseExpOpt with PrintExp 
-    with CompileScala { self => 
-  //override val verbosity = 1
-  val codegen = new ScalaGenNumericOps with ScalaGenStaticData with ScalaGenOrderingOps with ScalaGenArrayOps with ScalaGenRangeOps
-    with ScalaGenVariables with ScalaGenIfThenElse
-    with ScalaGenPrint /*with LivenessOpt*/ { val IR: self.type = self }
-  codegen.emitSource(test, "Test", new PrintWriter(System.out))
-  run()
-}
-trait Runner extends Compile {
-  def test(x: Rep[Array[Int]]): Rep[Array[Int]]
-  def run() {
-    val f = compile(test)
-    val v0 = Array(3, 1, 5, -2, 4)
-    val v1 = f(v0)
-    v1 foreach println
-  }
-}
-
-
 class TestHMM extends FileDiffSuite {
   
+  trait DSL extends LiftNumeric with NumericOps with ArrayOps with RangeOps with BooleanOps 
+    with LiftVariables with IfThenElse with Print {
+    def staticData[T:Manifest](x: T): Rep[T]
+    def test(x: Rep[Array[Int]]): Rep[Array[Int]]
+  }
+  trait Impl extends DSL with Runner with ArrayOpsExpOpt with NumericOpsExpOpt with OrderingOpsExpOpt with BooleanOpsExp 
+      with EqualExpOpt with VariablesExpOpt with RangeOpsExp with StaticDataExp
+      with IfThenElseExpOpt with PrintExp 
+      with CompileScala { self => 
+    //override val verbosity = 1
+    val codegen = new ScalaGenNumericOps with ScalaGenStaticData with ScalaGenOrderingOps with ScalaGenArrayOps with ScalaGenRangeOps
+      with ScalaGenVariables with ScalaGenIfThenElse
+      with ScalaGenPrint /*with LivenessOpt*/ { val IR: self.type = self }
+    codegen.emitSource(test, "Test", new PrintWriter(System.out))
+    run()
+  }
+  trait Runner extends Compile {
+    def test(x: Rep[Array[Int]]): Rep[Array[Int]]
+    def run() {
+      val f = compile(test)
+      val v0 = Array(3, 1, 5, -2, 4)
+      val v1 = f(v0)
+      v1 foreach println
+    }
+  }
+
+
+
+
   val prefix = "test-out/epfl/test11-"
   
   def testHmm1 = {
