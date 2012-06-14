161823cfb578ff5a3b0531c24f70d72fc47bcfdb
asujeeth <asujeeth@stanford.edu>
2012-06-13 19:09:37 -0700

    applying some of the performance optimizations from optiml-beta - provides about a 50% speedup on msmbuilder

--- src/internal/Scheduling.scala
+++ src/internal/Scheduling.scala
@@ -3,10 +3,9 @@ package internal
 
 import util.GraphUtil
 import scala.collection.mutable.HashMap
-
-
-
-
+import scala.collection.mutable.Set
+import java.util.IdentityHashMap
+import scala.collection.JavaConversions._
 
 trait Scheduling {
   val IR: Expressions
@@ -16,9 +15,25 @@ trait Scheduling {
     getSchedule(scope)(result, false)
   }
   
+  // PERFORMANCE: 'intersect' calls appear to be a hotspot
+
+  // checks if a and b share at least one element. O(N^2), but with no allocation and possible early exit.
+  def containsAny(a: List[Sym[Any]], b: List[Sym[Any]]): Boolean = {
+    var aIter = a.iterator
+    while (aIter.hasNext) {
+      val aElem = aIter.next()
+      val bIter = b.iterator
+      while (bIter.hasNext) {
+        if (bIter.next() eq aElem) return true
+      }
+    }
+    false
+  }
+   
   def getStronglySortedSchedule(scope: List[Stm])(result: Any): List[Stm] = {
     def deps(st: List[Sym[Any]]): List[Stm] = 
-      scope.filter(d => (st intersect d.lhs).nonEmpty)
+      scope.filter(d => containsAny(st, d.lhs))
+      // scope.filter(d => (st intersect d.lhs).nonEmpty)
     def allSyms(r: Any) = syms(r) ++ softSyms(r)
     
     val xx = GraphUtil.stronglyConnectedComponents[Stm](deps(allSyms(result)), t => deps(allSyms(t.rhs)))
@@ -33,7 +48,8 @@ trait Scheduling {
 
   def getSchedule(scope: List[Stm])(result: Any, sort: Boolean = true): List[Stm] = {
     def deps(st: List[Sym[Any]]): List[Stm] = 
-      scope.filter(d => (st intersect d.lhs).nonEmpty)
+      scope.filter(d => containsAny(st, d.lhs))
+      // scope.filter(d => (st intersect d.lhs).nonEmpty)
 
     val xx = GraphUtil.stronglyConnectedComponents[Stm](deps(syms(result)), t => deps(syms(t.rhs)))
     if (sort) xx.foreach { x => 
@@ -56,45 +72,72 @@ trait Scheduling {
     }
 
     def deps(st: List[Sym[Any]]): List[Stm] =
-      scope.filter(d => (st intersect d.lhs).nonEmpty)
+      scope.filter(d => containsAny(st, d.lhs))
+      // scope.filter(d => (st intersect d.lhs).nonEmpty)
 
     GraphUtil.stronglyConnectedComponents[Stm](deps(mysyms(result)), t => deps(mysyms(t.rhs))).flatten.reverse
   }
     
-
+  
+  /** begin performance hotspot **/
+  
   def getFatDependentStuff(scope: List[Stm])(sts: List[Sym[Any]]): List[Stm] = {
     /*
      precompute:
      s => all d in scope such that: d.lhs contains s || syms(d.rhs).contains(s)
      st => all d in scope such that: boundSyms(d.rhs) contains st
     */
-
-    val lhsCache = new HashMap[Sym[Any], List[Stm]]
-    val symsCache = new HashMap[Sym[Any], List[Stm]]
-    val boundSymsCache = new HashMap[Sym[Any], List[Stm]]
-
-    def putDef(map: HashMap[Sym[Any], List[Stm]], s: Sym[Any], d: Stm): Unit = {
-      map.getOrElse(s, Nil) match {
+    
+    // IdentityHashMap appears faster than scala.collection.mutable.HashMap here (based on perf. testing)
+    val lhsCache = new IdentityHashMap[Sym[Any], List[Stm]]()
+    val symsCache = new IdentityHashMap[Sym[Any], List[Stm]]()
+    val boundSymsCache = new IdentityHashMap[Sym[Any], Set[Stm]]()
+    
+    def getOrElse[K,V](map: IdentityHashMap[K, V], s: K)(f: => V) = {
+      var res = map.get(s)
+      if (res == null) res = f
+      res
+    }
+    
+    def putDef(map: IdentityHashMap[Sym[Any], List[Stm]], s: Sym[Any], d: Stm): Unit = {
+      var res = map.get(s)
+      if (res == null) res = Nil
+      // map.getOrElse(s, Nil) match {
+      res match {
         case `d`::ds =>
-        case ds => map(s) = d::ds
+        case ds => map.put(s,d::ds)
       }
     }
-
+    
+    def putDefSet(map: IdentityHashMap[Sym[Any], Set[Stm]], s: Sym[Any], d: Stm): Unit = {
+      var res = map.get(s)
+      if (res == null) {
+        res = Set[Stm]()
+        map.put(s,res)
+      }
+      res += d
+    }
+    
     for (d <- scope) {
       d.lhs.foreach(s => putDef(lhsCache, s, d))
-      syms(d.rhs).foreach(s => putDef(symsCache, s, d))
-      boundSyms(d.rhs).foreach(st => putDef(boundSymsCache, st, d))
+      syms(d.rhs).foreach(s => putDef(symsCache, s, d))      
+      boundSyms(d.rhs).foreach(st => putDefSet(boundSymsCache, st, d))      
     }
-
+            
     sts.distinct.flatMap { st =>
-      // could precompute uses as well...
+      // could also precalculate uses, but computing all combinations eagerly is also expensive
       def uses(s: Sym[Any]): List[Stm] = {
-        lhsCache.getOrElse(s, Nil) ++ (symsCache.getOrElse(s, Nil) filterNot (boundSymsCache.getOrElse(st, Nil) contains _))
+        getOrElse(lhsCache,s)(Nil) ::: getOrElse(symsCache,s)(Nil) filterNot (getOrElse(boundSymsCache,st)(Set.empty) contains _)
       }
-      GraphUtil.stronglyConnectedComponents[Stm](uses(st), t => t.lhs flatMap uses).flatten
-    }.distinct
-
-//    st.distinct.flatMap(getFatDependentStuff0(scope)(_)).distinct // this is expensive!!
+      GraphUtil.stronglyConnectedComponents[Stm](
+        uses(st),
+        t => t match {
+          case TP(s,d) => uses(s)
+          case _ => t.lhs flatMap uses
+        }).flatten
+    }.distinct    
   }
+  
+  /** end performance hotspot **/
 
 }
