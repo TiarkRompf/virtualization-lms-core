1db7d9f0e4b97d2a9b8d581b38e1cbc8f1f8c34d
HyoukJoong Lee <hyouklee@stanford.edu>
2012-07-18 18:06:29 -0700

    ArrayOps C++ codegen

--- src/common/ArrayOps.scala
+++ src/common/ArrayOps.scala
@@ -265,5 +265,17 @@ trait CLikeGenArrayOps extends BaseGenArrayOps with CLikeGenBase {
 
 trait CudaGenArrayOps extends CudaGenBase with CLikeGenArrayOps
 trait OpenCLGenArrayOps extends OpenCLGenBase with CLikeGenArrayOps
-trait CGenArrayOps extends CGenBase with CLikeGenArrayOps
+trait CGenArrayOps extends CGenBase with BaseGenArrayOps {
+  val IR: ArrayOpsExp
+  import IR._
+
+  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = {
+      rhs match {
+        case ArrayLength(x) => emitValDef(sym, quote(x) + "->length")
+        case ArrayApply(x,n) => emitValDef(sym, quote(x) + "->apply(" + quote(n) + ")")
+        case ArrayUpdate(x,n,y) => stream.println(quote(x) + "->update(" + quote(n) + "," + quote(y) + ");")
+        case _ => super.emitNode(sym, rhs)
+      }
+    }
+}
 
