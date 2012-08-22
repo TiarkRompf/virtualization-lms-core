0c2d15e8251231c37c3cb1157a993c8d4cbaba77
amin <nada.amin@epfl.ch>
2012-08-22 19:56:05 +0200

    use scalatest M7

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object LMSBuild extends Build {
   
   //val scalaTest = "org.scalatest" % "scalatest_2.10.0-virtualized-SNAPSHOT" % "1.6.1-SNAPSHOT" % "test"
   // TODO: use M7 version
-  val scalaTest = "org.scalatest" % "scalatest_2.10.0-M6" % "1.9-2.10.0-M6-B2" % "test"
+  val scalaTest = "org.scalatest" % "scalatest_2.10.0-M7" % "1.9-2.10.0-M7-B1" % "test"
   
   val virtScala = Option(System.getenv("SCALA_VIRTUALIZED_VERSION")).getOrElse("2.10.0-M7")
 
