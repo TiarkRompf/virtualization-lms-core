b587449150d3f7bb23b48d2f834293085d565e64
Tiark Rompf <tiark.rompf@epfl.ch>
2012-09-04 14:05:36 +0200

    sliding window stencil working, with expected output and bytes/flop ratio for shonan challenge problem

--- test-src/epfl/test11-shonan/TestStencil.scala
+++ test-src/epfl/test11-shonan/TestStencil.scala
@@ -20,26 +20,29 @@ class TestStencil extends FileDiffSuite {
     with BooleanOps with OrderingOps
     with LiftVariables with IfThenElse with Print {
     def staticData[T:Manifest](x: T): Rep[T]
-    def test(x: Rep[Array[Int]]): Rep[Array[Int]]
+    def infix_toDouble(x: Rep[Int]): Rep[Double]
+    def test(x: Rep[Array[Double]]): Rep[Array[Double]]
   }
   trait Impl extends DSL with Runner with ArrayOpsExpOpt with NumericOpsExpOpt 
       with OrderingOpsExpOpt with BooleanOpsExp with OrderingOpsExp
       with EqualExpOpt with VariablesExpOpt with RangeOpsExp with StaticDataExp
-      with IfThenElseExpOpt with PrintExp 
+      with IfThenElseExpOpt with PrintExp with PrimitiveOpsExp
       with CompileScala { self => 
     //override val verbosity = 1
+    def infix_toDouble(x: Rep[Int]): Rep[Double] = int_double_value(x)
+    
     val codegen = new ScalaGenNumericOps with ScalaGenStaticData with ScalaGenOrderingOps 
       with ScalaGenArrayOps with ScalaGenRangeOps with ScalaGenBooleanOps
-      with ScalaGenVariables with ScalaGenIfThenElse
+      with ScalaGenVariables with ScalaGenIfThenElse with ScalaGenPrimitiveOps
       with ScalaGenPrint /*with LivenessOpt*/ { val IR: self.type = self }
     dumpGeneratedCode = true
     run()
   }
   trait Runner extends Compile {
-    def test(x: Rep[Array[Int]]): Rep[Array[Int]]
+    def test(x: Rep[Array[Double]]): Rep[Array[Double]]
     def run() {
       val f = compile(test)
-      val v0 = Array(3, 1, 5, -2, 4)
+      val v0 = Array.tabulate(10)(i => 1.0 + 0.1 * i)
       val v1 = f(v0)
       v1 foreach println
     }
@@ -47,7 +50,12 @@ class TestStencil extends FileDiffSuite {
 
   trait Sliding extends DSL {
     
-    def infix_sliding(n: Rep[Int], f: Rep[Int] => Rep[Double]): Rep[Array[Double]]
+    def infix_sliding[T:Manifest](n: Rep[Int], f: Rep[Int] => Rep[T]): Rep[Array[T]] = {
+      sliding(NewArray[T](n),0,n)(f)
+    }
+
+    def sliding[T:Manifest](array: Rep[Array[T]], start: Rep[Int], end: Rep[Int])(f: Rep[Int] => Rep[T]): Rep[Array[T]]
+
     
   }
   
@@ -89,29 +97,55 @@ class TestStencil extends FileDiffSuite {
       }
     }
     
-    def infix_sliding[T:Manifest](n: Rep[Int], f: Rep[Int] => Rep[T]): Rep[Array[T]] = {
-      val a = NewArray[T](n)
-      
-      
+    // some arithemetic rewrites
+    override def numeric_plus[T:Numeric:Manifest](lhs: Exp[T], rhs: Exp[T])(implicit pos: SourceContext): Exp[T] = ((lhs,rhs) match {
+      case (Def(NumericMinus(x:Exp[Int],Const(y:Int))), Const(z:Int)) => if (y > z) numeric_minus(x, unit(y-z)) else numeric_plus(x,unit(z-y))  // (x-y)+z --> x-(y-z) or x+(z-y)
+      case _ => super.numeric_plus(lhs,rhs)
+    }).asInstanceOf[Exp[T]]
+    override def numeric_minus[T:Numeric:Manifest](lhs: Exp[T], rhs: Exp[T])(implicit pos: SourceContext): Exp[T] = ((lhs,rhs) match {
+      case (Def(NumericPlus(x:Exp[Int],Const(y:Int))), Const(z:Int)) => numeric_plus(x, unit(y-z)) // (x+y)-z --> x+(y-z)
+      case _ => super.numeric_minus(lhs,rhs)
+    }).asInstanceOf[Exp[T]]
+    
+    
+    // stencil implementation
+    def sliding[T:Manifest](a: Rep[Array[T]], start: Rep[Int], end: Rep[Int])(f: Rep[Int] => Rep[T]): Rep[Array[T]] = {
       val i = fresh[Int]
       
-      val ((r0,r1,stms1,subst), stms0) = reifySubGraph {
+      // evaluate loop contents f(i)
+      val (r0,stms0) = reifySubGraph(f(i))
+      
+      val (((r1,stms1,subst1),(r2,stms2,subst2)), _) = reifySubGraph {
         
-        // evaluate loop contents f(i)
-        val (r0,stms0) = reifySubGraph(f(i))
         reflectSubGraph(stms0)
         
         val iPlusOne = i+1
         
         // evaluate loop contents f(i+1)
-        val ((r1,subst),stms1) = reifySubGraph(trans.withSubstScope(i -> iPlusOne) {
+        val ((r1,subst1),stms1) = reifySubGraph(trans.withSubstScope(i -> iPlusOne) {
           stms0.foreach(s=>trans.traverseStm(s))
           (trans(r0), trans.subst)
         })
         
-        println(subst)
+
+        val ((r2,stms2,subst2), _) = reifySubGraph {
+
+          reflectSubGraph(stms1)
+
+          val iPlusTwo = iPlusOne+1
+          
+          // evaluate loop contents f(i+2)
+          val ((r2,subst2),stms2) = reifySubGraph(trans.withSubstScope(i -> iPlusTwo) {
+            stms0.foreach(s=>trans.traverseStm(s))
+            (trans(r0), trans.subst)
+          })
+          (r2,stms2,subst2)
+        }
+
+        println(subst1)
         
-        (r0,r1,stms1,subst)
+        ((r1,stms1,subst1), (r2,stms2,subst2))
+
       }
 
       val defs = stms0.flatMap(_.lhs)
@@ -121,63 +155,78 @@ class TestStencil extends FileDiffSuite {
 
       println("r1:")
       stms1.foreach(println)
+
+      println("r2:")
+      stms2.foreach(println)
           
-      println(subst)
+      println(subst1)
         
 
-      // find overlap syms: defined by f(i), used by f(i+1)
+      // find overlap syms: defined by f(i), used by f(i+1) and f(i+2)
 
-      val overlap0 = stms1.flatMap { case TP(s,d) => syms(d) filter (defs contains _) }.distinct
-      val overlap1 = overlap0 map subst
+      val overlap01 = stms1.flatMap { case TP(s,d) => syms(d) filter (defs contains _) }.distinct
+      val overlap02 = stms2.flatMap { case TP(s,d) => syms(d) filter (defs contains _) }.distinct
 
-      overlap0 zip overlap1 foreach println
+      val overlap0 = (stms1++stms2).flatMap { case TP(s,d) => syms(d) filter (defs contains _) }.distinct
+      val overlap1 = overlap0 map subst1
+      val overlap2 = overlap0 map subst2
 
+      println("overlap1:")
+      overlap01 map (x=>(x,subst1(x))) foreach println
+
+      println("overlap2:")
+      overlap02 map (x=>(x,subst2(x))) foreach println
+
+
+      if (overlap02.nonEmpty)
+        println("WARNING: overlap beyond a single loop iteration not yet implemented")
 
       // build a variable for each overlap sym.
       // init variables by peeling first loop iteration.
       
-      // if (n > 0) {}
+      if (end > start) {
 
-      val (rX,substX) = trans.withSubstScope(i -> unit(0)) {
-        stms0.foreach(s=>trans.traverseStm(s))
-        (trans(r0), trans.subst)
-      }
-
-      val vars = overlap0 map (x => var_new(substX(x))(x.tp,x.pos.head))
-
-      a(unit(0)) = rX
-
-      println("var inits: " + overlap0 + " -> " + vars)
-      println("will become var reads: " + overlap0)
-      println("will become var writes: " + overlap1)
-
-      
-      // now generate the loop
-      
-      for (j <- unit(1) until (n)) {
-        
-        // read the overlap variables
-        
-        val reads = (overlap0 zip vars) map (p => (p._1, readVar(p._2)))
-        
-        println("var reads: " + reads)
-        
-        // emit the transformed loop body
-        
-        val (r,jPlusOne,subst2) = trans.withSubstScope((reads:+((i+1)->j)): _*) {
-          stms1.foreach(s=>trans.traverseStm(s))
-          (trans(r1), trans(i+1), trans.subst)
+        val (rX,substX) = trans.withSubstScope(i -> start) {
+          stms0.foreach(s=>trans.traverseStm(s))
+          (trans(r0), trans.subst)
         }
-        
-        // write the new values to the overlap vars
-        
-        val writes = (overlap1 zip vars) map (p => (p._1, var_assign(p._2, subst2(p._1))))
 
-        println("var writes: " + writes)
-        
-        a(j) = r
-      }
+        val vars = overlap0 map (x => var_new(substX(x))(x.tp,x.pos.head))
+
+        a(start) = rX
+
+        println("var inits: " + overlap0 + " -> " + vars)
+        println("will become var reads: " + overlap0)
+        println("will become var writes: " + overlap1)
+
       
+        // now generate the loop
+      
+        for (j <- (start+unit(1)) until end) {
+        
+          // read the overlap variables
+        
+          val reads = (overlap0 zip vars) map (p => (p._1, readVar(p._2)))
+        
+          println("var reads: " + reads)
+        
+          // emit the transformed loop body
+        
+          val (r,jPlusOne,substY1) = trans.withSubstScope((reads:+((i+1)->j)): _*) {
+            stms1.foreach(s=>trans.traverseStm(s))
+            (trans(r1), trans(i+1), trans.subst)
+          }
+        
+          // write the new values to the overlap vars
+        
+          val writes = (overlap1 zip vars) map (p => (p._1, var_assign(p._2, substY1(p._1))))
+
+          println("var writes: " + writes)
+        
+          a(j) = r
+        }
+      
+      }
       a
     }
     
@@ -199,11 +248,11 @@ class TestStencil extends FileDiffSuite {
         a
       }
       
-      def test(v: Rep[Array[Int]]) = {
+      def test(v: Rep[Array[Double]]) = {
 
         val n = 20
         
-        def compute(i: Rep[Int]) = 2 * i + 3
+        def compute(i: Rep[Int]) = 2.0 * i.toDouble + 3.0
         
         val res = n sliding { i =>
           compute(i) + compute(i+1)
@@ -218,11 +267,11 @@ class TestStencil extends FileDiffSuite {
 
   def testStencil1 = withOutFileChecked(prefix+"stencil1") {
     trait Prog extends DSL with Sliding {
-      def test(v: Rep[Array[Int]]) = {
+      def test(v: Rep[Array[Double]]) = {
 
         val n = 20
         
-        def compute(i: Rep[Int]) = 2 * i + 3
+        def compute(i: Rep[Int]) = 2.0 * i.toDouble + 3.0
         
         val res = n sliding { i =>
           compute(i) + compute(i+1)
@@ -236,99 +285,79 @@ class TestStencil extends FileDiffSuite {
 
 
 
-/*
-  def testStencil2 = withOutFileChecked(prefix+"stencil2") {
-    trait Prog {
-      type Rep[T]
-      def test(v: Rep[Array[Int]]) = {
-      }
+  /*
+  const int N = 20;
+  void program0(vector<double> input, vector<double>& output) {
+    vector<double> input2(N);
+    vector<double> wind(N);
+    for (int i = 0; i < N; ++i) {
+      input2[i]=input[i];
+    }
+    for (int i = 0; i < N-1; ++i) {
+      wind[i] = input2[i] * input2[i+1];
+    }
+    for (int i = 1; i < N+1; ++i) {
+      output[i] = input2[i] - wind[i] + wind[i-1];
     }
   }
-
-*/
+  */
 
   def testStencil2 = withOutFileChecked(prefix+"stencil2") {
     trait Prog extends DSL with Sliding {
-      def test(v: Rep[Array[Int]]) = {
+      def test(v: Rep[Array[Double]]) = {
 
-        /*
-        const int N = 20;
-        void program0(vector<double> input, vector<double>& output) {
-          vector<double> input2(N);
-          vector<double> wind(N);
-          for (int i = 0; i < N; ++i) {
-            input2[i]=input[i];
-          }
-          for (int i = 0; i < N-1; ++i) {
-            wind[i] = input2[i] * input2[i+1];
-          }
-          for (int i = 1; i < N+1; ++i) {
-            output[i] = input2[i] - wind[i] + wind[i-1];
-          }
+        val n = v.length
+        val input = v
+        val output = NewArray[Double](v.length)
+        for (i <- 0 until n) output(i) = -1.0
+        
+
+        def a(j: Rep[Int]) = input(j) //if (j > 0 && j < input.length) input(j) else 0
+        
+        def w(j: Rep[Int]) = a(j) * a(j+1)
+
+        
+        // result has 1 read, 1 write, 3 flops = 16/3 bytes/flop
+
+        sliding(output,1,n-1) { i =>
+          a(i) - w(i) + w(i-1)
         }
-        */
-        
-        /*
-        w = a_0 * a_1
-        b = a_0 - w_0 + w_(-1)
+      }
+    }
+    new Prog with Impl with SlidingExp
+  }
 
+  def testStencil3 = withOutFileChecked(prefix+"stencil3") {
+    trait Prog extends DSL with Sliding {
+      def test(v: Rep[Array[Double]]) = {
 
-        w1 = a_0 * a_1
-        wm = a_0 - w1_0 + w1_(-1)
-        w2 = wm_0 * wm_1
-        b  = wm_0 - w2_0 + w2_(-1)
-        */
-        
-        
-        
-        
-        /*
-        .~(loop0 2 0 n .<a>. .<b>. (fun a ->
-        let w1 j = a j *@ a (j+1) in 
-        let wm j = a j -@ w1 j +@ w1 (j-1) in 
-        let w2 j = wm j *@ wm (j+1) in 
-        wm 0 -@ w2 0 +@ w2 (-1)))
-        */
-        
-        /*
-        
-        def compute(b: Rep[Array[Int]], a: Rep[Array[Int]], n: Rep[Int]) = {
-          def w(j: Rep[Int]) = a(j) * a(j+1)
-          for (i <- 0 until n: Rep[Range]) {
-            b(i) = a(i) - w(i) + w(i-1)
-          }
-        }
-        */
-
-        
-        /*def compute(a: Rep[Array[Int]], n: Rep[Int]) = {
-          def w(j: Rep[Int]) = a(j) * a(j+1)
-          n sliding { i =>
-            a(i) - w(i) + w(i-1)
-          }
-        }*/
         val n = v.length
         val input = v
 
-        def a(j: Rep[Int]) = if (j > 0 && j < input.length) input(j) else 0
+        val output = NewArray[Double](v.length)
+        for (i <- 0 until n) output(i) = -1.0
         
-        def w(j: Rep[Int]): Rep[Int] = a(j) * a(j+1)
+        // perform two iterations at once
 
-        val b = n sliding { i =>
-          a(i) - w(i) + w(i-1)
-        }
+        def a(j: Rep[Int]) = input(j) //if (j > 0 && j < input.length) input(j) else 0.0
         
-        b
+        def w1(j: Rep[Int]) = a(j) * a(j+1)
+
+        def wm(j: Rep[Int]) = a(j) - w1(j) + w1(j-1)
+
+        def w2(j: Rep[Int]) = wm(j) * wm(j+1)
+        
+        def b(j: Rep[Int]) = wm(j) - w2(j) + w2(j-1)
+
+        // result has 1 read, 1 write, 6 flops = 16/6 bytes/flop
+        
+        sliding(output,2,n-2) { i =>
+          b(i)
+        }
       }
     }
-    new Prog with Impl with SlidingExp {
-      override def numeric_plus[T:Numeric:Manifest](lhs: Exp[T], rhs: Exp[T])(implicit pos: SourceContext): Exp[T] = ((lhs,rhs) match {
-        case (Def(NumericMinus(x:Exp[Int],Const(y:Int))), Const(z:Int)) => if (y > z) numeric_minus(x, y-z) else numeric_plus(x,z-y) // x-y+z  x-3+2=x-(3-2) x-2+3=x+3-2
-        case _ => super.numeric_plus(lhs,rhs)
-      }).asInstanceOf[Exp[T]]
-    }
+    new Prog with Impl with SlidingExp
   }
-
 
  
 }
