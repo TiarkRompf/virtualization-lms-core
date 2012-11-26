e7c659debe6ac1d9d97003ba9d8b0f31a723818d
Tiark Rompf <tiark.rompf@epfl.ch>
2012-11-26 15:37:07 +0100

    unchecked for C

--- src/common/Unchecked.scala
+++ src/common/Unchecked.scala
@@ -55,3 +55,13 @@ trait ScalaGenUncheckedOps extends ScalaGenBase {
   }
 }
 
+trait CGenUncheckedOps extends CGenBase {
+  val IR: UncheckedOpsExp
+  import IR._
+
+  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+    case Unchecked(xs) => 
+      emitValDef(sym, xs map ((x:Any)=> x match { case x: Exp[_] => quote(x) case x => x.toString }) mkString "")
+    case _ => super.emitNode(sym, rhs)
+  }
+}
