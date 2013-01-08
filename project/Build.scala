8c1e45e37044b6a7bec5c2c401e7e1f3d3356f67
amin <nada.amin@epfl.ch>
2013-01-08 17:42:37 +0100

    update scala test

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object LMSBuild extends Build {
 
   //val prereleaseScalaTest = "Pre-Release ScalaTest" at "https://scala-webapps.epfl.ch/jenkins/view/2.10.x/job/community-nightly-2.10.0/ws/target/repositories/bccb0f4eb0bf6024577064915da437e3574281cb/"
 
-  val scalaTest = "org.scalatest" % "scalatest_2.10.0-RC2" % "2.0.M4" % "test" //"org.scalatest" % "scalatest_2.10.0-M7" % "1.9-2.10.0-M7-B1" % "test"
+  val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test"
   
   val virtScala = Option(System.getenv("SCALA_VIRTUALIZED_VERSION")).getOrElse("2.10.0")
 
