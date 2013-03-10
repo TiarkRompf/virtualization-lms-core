4623ae25198a57005a22bde46dea1f499eb3a709
Arvind Sujeeth <asujeeth@stanford.edu>
2013-03-10 14:20:56 -0700

    fixing dropboxScalaTestRepo

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import java.io.File
 
 object LMSBuild extends Build {
   // FIXME: custom-built scalatest
-  val dropboxScalaTestRepo = "Dropbox" at "http://dl.dropbox.com/u/12870350/scala-virtualized"
+  val dropboxScalaTestRepo = "Dropbox" at "http://dl.dropbox.com/u/1186811/scala-virtualized"
   val scalaTest = "org.scalatest" % "scalatest_2.10.0-virtualized-SNAPSHOT" % "1.6.1-SNAPSHOT" % "test"
   val virtScala = Option(System.getenv("SCALA_VIRTUALIZED_VERSION")).getOrElse("2.10.0-M1-virtualized")
 
