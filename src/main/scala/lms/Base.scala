d02c9ad873ef2f10a7e2c0a5cbcb77e9e26ddfc1
Sandro Stucki <sandro.stucki@gmail.com>
2013-04-24 17:52:30 +0200

    Moves definitions of C-like *Gen{Base,Effect,Fat} classes to targe/.

--- src/main/scala/lms/Base.scala
+++ src/main/scala/lms/Base.scala
@@ -88,28 +88,5 @@ trait BaseFatExp extends BaseExp with FatExpressions with FatTransforming
 // not need to depend on internal._
 
 trait ScalaGenBase extends ScalaCodegen
-
 trait ScalaGenEffect extends ScalaNestedCodegen with ScalaGenBase
-
 trait ScalaGenFat extends ScalaFatCodegen with ScalaGenBase
-
-// FIXME: add this to target/
-trait CLikeGenBase extends CLikeCodegen
-trait CLikeGenEffect extends CLikeNestedCodegen with CLikeGenBase
-trait CLikeGenFat extends CLikeFatCodegen with CLikeGenBase
-
-trait GPUGenBase extends GPUCodegen
-trait GPUGenEffect extends GPUGenBase with CLikeNestedCodegen
-trait GPUGenFat extends GPUGenBase with CLikeFatCodegen
-
-trait CudaGenBase extends CudaCodegen
-trait CudaGenEffect extends CudaNestedCodegen with CudaGenBase
-trait CudaGenFat extends CudaFatCodegen with CudaGenBase
-
-trait OpenCLGenBase extends OpenCLCodegen
-trait OpenCLGenEffect extends OpenCLNestedCodegen with OpenCLGenBase
-trait OpenCLGenFat extends OpenCLFatCodegen with OpenCLGenBase
-
-trait CGenBase extends CCodegen
-trait CGenEffect extends CNestedCodegen with CGenBase
-trait CGenFat extends CFatCodegen with CGenBase
