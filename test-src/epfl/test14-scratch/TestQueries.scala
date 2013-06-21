5ad9db8c6beadbe5a2284f556b9a159b5b076f5a
Tiark Rompf <tiark.rompf@epfl.ch>
2013-06-21 20:57:45 +0200

    query test file skeleton

---
+++ test-src/epfl/test14-scratch/TestQueries.scala
@@ -0,0 +1,69 @@
+package scala.virtualization.lms
+package epfl
+package test14
+
+import common._
+import internal._
+
+import util.OverloadHack
+
+import java.io.{PrintWriter,StringWriter,FileOutputStream}
+import scala.reflect.SourceContext
+
+/*
+Staged SQL-like queries, inspired by "the essence of LINQ":
+http://homepages.inf.ed.ac.uk/slindley/papers/essence-of-linq-draft-december2012.pdf
+*/
+
+class TestQueries extends FileDiffSuite {
+  
+  val prefix = "test-out/epfl/test14-"
+  
+  trait DSL extends ScalaOpsPkg with Compile with LiftPrimitives {
+    def test(): Unit
+  }
+  
+  trait Impl extends DSL with ScalaOpsPkgExp with ScalaCompile { self =>
+    override val verbosity = 1
+    dumpGeneratedCode = true
+    val codegen = new Codegen { val IR: self.type = self }
+    val runner = new Runner { val p: self.type = self }
+    runner.run()
+  }
+  
+  trait Codegen extends ScalaCodeGenPkg {
+    val IR: Impl
+  }  
+  
+  trait Runner {
+    val p: Impl
+    def run() = {
+      p.test()
+    }
+  }
+  
+
+
+  def testQueries1 = withOutFileChecked(prefix+"queries1") {
+    trait Prog extends DSL {
+      def test() = {
+
+        val f = compile { x: Rep[Int] =>
+
+          val a = x + 1
+          val b = x * 2
+
+          a+b
+        }
+
+        Console.println(f(9))
+        Console.println(f(3))
+        Console.println(f(1))
+
+      }
+    }
+    new Prog with Impl
+  }
+
+
+}
