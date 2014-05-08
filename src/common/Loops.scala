1ea48edb1f8cc51d30b4b90640afb2f3c45f8f12
Jithin Thomas <jithinpt@Jithins-MacBook-Pro.local>
2014-05-08 11:51:38 -0700

    SourceContext fix. The simpleLoop method is called while performing MultiLoopSoa transforms

--- src/common/Loops.scala
+++ src/common/Loops.scala
@@ -19,7 +19,7 @@ trait LoopsExp extends Loops with BaseExp with EffectExp {
   
   case class SimpleLoop[A](val size: Exp[Int], val v: Sym[Int], val body: Def[A]) extends AbstractLoop[A]
   
-  def simpleLoop[A:Manifest](size: Exp[Int], v: Sym[Int], body: Def[A]): Exp[A] = SimpleLoop(size, v, body)
+  def simpleLoop[A:Manifest](size: Exp[Int], v: Sym[Int], body: Def[A])(implicit pos: SourceContext): Exp[A] = SimpleLoop(size, v, body)
 
 
   override def syms(e: Any): List[Sym[Any]] = e match {
