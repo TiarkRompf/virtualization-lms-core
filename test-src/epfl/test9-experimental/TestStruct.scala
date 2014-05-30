b3baf92b5451544257ba52694fffa84a7a1bf9f6
Jithin Thomas <jithinpt@Jithins-MacBook-Pro.local>
2014-05-29 17:06:55 -0700

    Fixing some tests that were failing compilation due to changes in the signature of reflectMirrored()

--- test-src/epfl/test9-experimental/TestStruct.scala
+++ test-src/epfl/test9-experimental/TestStruct.scala
@@ -35,9 +35,10 @@ trait StructExpOptLoops extends StructExpOptCommon with ArrayLoopsExp {
   
   case class ArraySoaTag[T](base: StructTag[T], len: Exp[Int]) extends StructTag[T]
   
-  override def simpleLoop[A:Manifest](size: Exp[Int], v: Sym[Int], body: Def[A]): Exp[A] = body match {
+  //override def simpleLoop[A:Manifest](size: Exp[Int], v: Sym[Int], body: Def[A]): Exp[A] = body match {
+  override def simpleLoop[A:Manifest](size: Exp[Int], v: Sym[Int], body: Def[A])(implicit pos: SourceContext): Exp[A] = body match {
     case ArrayElem(Block(Def(Struct(tag:StructTag[A], elems)))) => 
-      struct[A](ArraySoaTag[A](tag,size), elems.map(p=>(p._1,simpleLoop(size, v, ArrayElem(Block(p._2)))(p._2.tp.arrayManifest))))
+      struct[A](ArraySoaTag[A](tag,size), elems.map(p=>(p._1,simpleLoop(size, v, ArrayElem(Block(p._2)))(p._2.tp.arrayManifest, pos))))
     case ArrayElem(Block(Def(ArrayIndex(b,`v`)))) if infix_length(b) == size => b.asInstanceOf[Exp[A]] 
     // eta-reduce! <--- should live elsewhere, not specific to struct
     // rewrite loop(a.length) { i => a(i) } to a
