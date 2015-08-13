f1788114f5cc3e2e2a6d585fe0e84f6222917b7d
Tiark Rompf <tiark.rompf@epfl.ch>
2015-08-13 14:16:36 +0200

    test list ops

--- test-src/epfl/test12-collections/TestList.scala
+++ test-src/epfl/test12-collections/TestList.scala
@@ -25,7 +25,7 @@ class TestList extends FileDiffSuite {
       List[Int]() ++ xs // VIRT 2.11: need type annotation
 
     def emptyRight(xs: Rep[List[Int]]): Rep[List[Int]] =
-      xs ++ List()
+      xs ++ List[Int]()
   }
 
   trait MkString { this: ListOps =>
@@ -39,6 +39,7 @@ class TestList extends FileDiffSuite {
     withOutFile(prefix+"map-flatmap-filter") {
       val prog = new MapFlatMapAndFilter with ListOpsExp with NumericOpsExp with PrimitiveOpsExp with OrderingOpsExp
       val codegen = new ScalaGenEffect with ScalaGenListOps with ScalaGenNumericOps with ScalaGenPrimitiveOps with ScalaGenOrderingOps { val IR: prog.type = prog }
+      import prog.{intTyp,unitTyp,listTyp}
       codegen.emitSource(prog.test, "MapFlatMapAndFilter", new PrintWriter(System.out))
     }
     assertFileEqualsCheck(prefix+"map-flatmap-filter")
@@ -48,6 +49,7 @@ class TestList extends FileDiffSuite {
     withOutFile(prefix+"concat") {
       val prog = new Concat with ListOpsExpOpt
       val codegen = new ScalaGenEffect with ScalaGenListOps { val IR: prog.type = prog }
+      import prog.{intTyp,unitTyp,listTyp}
       codegen.emitSource(prog.test, "Concat", new PrintWriter(System.out))
       codegen.emitSource(prog.emptyLeft, "ConcatEmptyLeft", new PrintWriter(System.out))
       codegen.emitSource(prog.emptyRight, "ConcatEmptyRight", new PrintWriter(System.out))
@@ -59,6 +61,7 @@ class TestList extends FileDiffSuite {
     withOutFile(prefix+"mkstring") {
       val prog = new MkString with ListOpsExp
       val codegen = new ScalaGenEffect with ScalaGenListOps { val IR: prog.type = prog }
+      import prog.{intTyp,unitTyp,listTyp,stringTyp}
       codegen.emitSource(prog.test, "MkString", new PrintWriter(System.out))
     }
     assertFileEqualsCheck(prefix+"mkstring")
