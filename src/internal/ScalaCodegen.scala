6c8e5b86e03abb91126796c83dda5a343c77f7d5
Tiark Rompf <tiark.rompf@epfl.ch>
2015-08-13 03:26:11 +0200

    scala codegen

--- src/internal/ScalaCodegen.scala
+++ src/internal/ScalaCodegen.scala
@@ -21,9 +21,9 @@ trait ScalaCodegen extends GenericCodegen with Config {
       outFile.delete
   }
 
-  def emitSource[A : Typ](args: List[Sym[_]], body: Block[A], className: String, out: PrintWriter) = {
+  def emitSource[A:Typ](args: List[Sym[_]], body: Block[A], className: String, out: PrintWriter) = {
 
-    val sA = remap(manifest[A])
+    val sA = remap(typ[A])
 
     val staticData = getFreeDataBlock(body)
 
