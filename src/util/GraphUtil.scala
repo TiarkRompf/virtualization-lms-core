fdd172c3ec0c8a9158fed656ef29ed920d70ca60
David Koeplinger <dkoeplin@stanford.edu>
2016-05-12 11:16:21 -0700

    added least common ancestor to GraphUtil

--- src/util/GraphUtil.scala
+++ src/util/GraphUtil.scala
@@ -5,11 +5,29 @@ import java.util.{ArrayDeque, HashMap}
 
 
 object GraphUtil {
-  
+
   class Ref[T](init: T) {
     var value: T = init
   }
-  
+
+  /**
+      Returns the least common ancestor of two nodes in some directed, acyclic graph
+
+   */
+  def leastCommonAncestor[T](x: T, y: T, parent: T => Option[T]): Option[T] = {
+    var pathX: List[Option[T]] = List(Some(x))
+    var pathY: List[Option[T]] = List(Some(y))
+
+    var curX = Some(x)
+    while (curX.isDefined) { curX = parent(curX); pathX ::= curX }
+
+    var curY = Some(y)
+    while (curY.isDefined) { curY = parent(curY); pathY ::= curY }
+
+    // Choose last node where paths are the same
+    pathX.zip(pathY).filter{case (x,y) => x == y}.last
+  }
+
   /* test cases
 
      stronglyConnectedComponents[String](List("A"), { case "A" => List("B") case "B" => List("C") case "C" => List("A","D") case "D" => Nil})
@@ -18,7 +36,7 @@ object GraphUtil {
      stronglyConnectedComponents[String](List("A","B","C"), { case "A" => List("B") case "B" => List("C") case "C" => List("A","D") case "D" => Nil})
   */
 
-  /** 
+  /**
       Returns the strongly connected components
       of the graph rooted at the first argument,
       whose edges are given by the function argument.
@@ -39,11 +57,11 @@ object GraphUtil {
     res.value
   }
 
-  def visit[T](node: T, succ: T=>List[T], id: Ref[Int], stack: ArrayDeque[T], 
+  def visit[T](node: T, succ: T=>List[T], id: Ref[Int], stack: ArrayDeque[T],
             mark: HashMap[T,Int], res: Ref[List[List[T]]]): Int = {
 
-    
-    if (mark.containsKey(node)) 
+
+    if (mark.containsKey(node))
       mark.get(node)
     else {
       id.value = id.value + 1
@@ -56,7 +74,7 @@ object GraphUtil {
       for (child <- succ(node)) {
         val m = visit(child, succ, id, stack, mark, res)
 
-        if (m < min) 
+        if (m < min)
           min = m
       }
 
@@ -75,5 +93,5 @@ object GraphUtil {
       min
     }
   }
-  
+
 }
