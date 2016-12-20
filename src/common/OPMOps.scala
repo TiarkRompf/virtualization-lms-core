0988ec338c7099050fffc93b14d685f68178c9d7
James Decker <decker31@purdue.edu>
2016-12-20 11:25:05 -0500

    Fixes

--- src/common/OPMOps.scala
+++ src/common/OPMOps.scala
@@ -1,10 +1,9 @@
-
-package scala.virtualization.lms
+package scala.lms
 package common
 
 import java.io.PrintWriter
 
-import scala.virtualization.lms.internal.{GenericNestedCodegen, GenerationFailedException}
+import internal.{GenericNestedCodegen, GenerationFailedException}
 import scala.reflect.SourceContext
 
 trait OMPOps extends Base {
